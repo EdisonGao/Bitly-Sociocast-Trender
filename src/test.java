c254e59dd69c4b73452b828c7cbed7a100c66855
Shivam Naik <shivam.naik@gmail.com>
2013-06-15 14:10:40 -0400

    update

--- src/test.java
+++ src/test.java
@@ -1,4 +1,16 @@
+import com.rosaloves.bitlyj.*;
+
+import static com.rosaloves.bitlyj.Bitly.*;
 
 public class test {
 
+	
+	public static void main(String[] args){
+		UrlClicks url = as("o_7h53qc7l66", "R_30b1af9c47025baa7225399bf53aa21c").call(clicks(" http://bit.ly/19DiAV6"));
+		
+		System.out.println(url.getUserClicks());
+	}
+
 }
+
+
